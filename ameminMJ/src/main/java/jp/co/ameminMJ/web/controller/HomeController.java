4eadf05b6df3953e3e67672d6be121874c351bcd
Masaki <Masaki@Masaki-PC>
2017-02-25 20:32:37 +0900

    request response

--- ameminMJ/src/main/java/jp/co/ameminMJ/web/controller/HomeController.java
+++ ameminMJ/src/main/java/jp/co/ameminMJ/web/controller/HomeController.java
@@ -1,5 +1,8 @@
 package jp.co.ameminMJ.web.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -13,7 +16,7 @@ public class HomeController {
 	
 	
 	@RequestMapping("/home")
-	public String execute(@ModelAttribute LoginForm loginForm, Model model){
+	public String execute(@ModelAttribute LoginForm loginForm,HttpServletRequest request,HttpServletResponse response, Model model){
 		return("home");
 		
 	}
