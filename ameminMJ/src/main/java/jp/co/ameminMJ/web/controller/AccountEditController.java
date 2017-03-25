bc4f7a50f11bc6fd920c42633fe9f9a321047c41
SuperTed922 <11ed105a@rikkyo.ac.jp>
2017-03-25 16:49:57 +0900

    20170325_アカウント編集画面足しました_oguma

---
+++ ameminMJ/src/main/java/jp/co/ameminMJ/web/controller/AccountEditController.java
@@ -0,0 +1,7 @@
+package jp.co.ameminMJ.web.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class AccountEditController {
+}
--- ameminMJ/src/main/java/jp/co/ameminMJ/web/controller/HomeController.java
+++ ameminMJ/src/main/java/jp/co/ameminMJ/web/controller/HomeController.java
@@ -8,12 +8,17 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import jp.co.ameminMJ.web.form.AccountEditForm;
 import jp.co.ameminMJ.web.form.BattleHomeForm;
 import jp.co.ameminMJ.web.form.DeckHomeForm;
 import jp.co.ameminMJ.web.form.GachaHomeForm;
 
 @Controller
 public class HomeController {
+	@RequestMapping("/accountEdit")
+	public String execute(@ModelAttribute AccountEditForm accountEditForm,HttpServletRequest request,HttpServletResponse response, Model model){
+		return("accountEdit");
+	}
 	@RequestMapping("/deckHome")
 	public String execute(@ModelAttribute DeckHomeForm DeckHomeForm,HttpServletRequest request,HttpServletResponse response, Model model){
 		return("deckHome");
