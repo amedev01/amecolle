4ec8ed40550d4da2217febddcbe434ad113308f4
Masaki <Masaki@Masaki-PC>
2017-08-11 17:38:51 +0900

    deckHome

--- src/main/java/jp/co/amedev/amecolle/repository/DeckRepository.java
+++ src/main/java/jp/co/amedev/amecolle/repository/DeckRepository.java
@@ -1,5 +1,7 @@
 package jp.co.amedev.amecolle.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +10,7 @@ import jp.co.amedev.amecolle.repository.entity.DeckEntity;
 @Repository
 public interface DeckRepository extends JpaRepository<DeckEntity, Long> {
 	DeckEntity findOneById(long id);
+	
+	List<DeckEntity> findByIdNot(long id);
 	
 }
